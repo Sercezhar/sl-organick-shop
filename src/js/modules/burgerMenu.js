878144ced871818bbb5cefc7b8a5e0b9aad4be50
Oleksandr Havryliuk <deolis.ua@gmail.com>
2023-09-04 16:43:03 +0300

    update burgerMenu.js

--- src/js/modules/burgerMenu.js
+++ src/js/modules/burgerMenu.js
@@ -1,28 +1,26 @@
 export function burgerMenu() {
-  const burger = document.querySelector('.header__burger');
-  const menu = document.querySelector('.menu');
+  const btnOpen = document.querySelector('.header__burger');
+  const btnClose = document.querySelector('.menu__btn-close');
+  const menu = document.querySelector('.menu__backdrop');
   const body = document.querySelector('body');
 
-  burger.addEventListener('click', () => {
-    if (!menu.classList.contains('menu--active')) {
-      // open menu
-      menu.classList.add('menu--active');
-      // animate burger
-      burger.classList.add('header__burger--active');
-      // lock scroll
-      body.classList.add('locked');
-    } else {
-      menu.classList.remove('menu--active');
-      burger.classList.remove('header__burger--active');
-      body.classList.remove('locked');
-    }
-  });
-  // navbar breakpoint
-  // window.addEventListener('resize', () => {
-  //   if (window.innerWidth > 991.98) {
-  //     menu.classList.remove('active');
-  //     burger.classList.remove('active');
-  //     body.classList.remove('locked');
-  //   }
-  // });
+  window.addEventListener(
+    'resize',
+    () => window.innerWidth > 1360 && closeMenu()
+  );
+  btnOpen.addEventListener('click', openMenu);
+  btnClose.addEventListener('click', closeMenu);
+  menu.addEventListener('click', closeMenu);
+
+  function openMenu() {
+    menu.classList.add('menu__backdrop--active');
+    btnOpen.classList.add('header__burger--active');
+    body.classList.add('locked');
+  }
+
+  function closeMenu() {
+    menu.classList.remove('menu__backdrop--active');
+    btnOpen.classList.remove('header__burger--active');
+    body.classList.remove('locked');
+  }
 }
