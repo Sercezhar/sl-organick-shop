3fe492c28c2c060f062829bc6ff2a7af983f56b0
Oleksandr Havryliuk <deolis.ua@gmail.com>
2023-09-07 18:49:04 +0300

    add counter

---
+++ src/js/modules/counter.js
@@ -0,0 +1,39 @@
+export function counterOnScroll() {
+  function counterFunc(counterSelector, attributeSelector) {
+    const counters = document.querySelectorAll(counterSelector);
+
+    const interval = 2000;
+
+    counters.forEach(counter => {
+      let startsValue = 0;
+      const endValue = parseInt(counter.getAttribute(attributeSelector));
+      const duration = Math.floor(interval / endValue);
+      console.log(endValue, duration);
+      const counterHandler = setInterval(() => {
+        startsValue += 1;
+        counter.textContent = startsValue;
+
+        if (startsValue == endValue) {
+          clearInterval(counterHandler);
+        }
+      }, duration);
+    });
+  }
+
+  // play animation on scroll
+  const counterSection = document.querySelectorAll('.counters');
+
+  function counterTrigger(entries) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        counterFunc('.counters__number', 'data-number');
+      }
+    });
+  }
+
+  const observer = new IntersectionObserver(counterTrigger);
+
+  counterSection.forEach(box => {
+    observer.observe(box);
+  });
+}
