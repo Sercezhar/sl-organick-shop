e818844c47fdf5980470013f17046211f1489320
Oleksandr Havryliuk <deolis.ua@gmail.com>
2023-09-10 01:53:29 +0300

    add about page

--- gulp/config/webpack.config.js
+++ gulp/config/webpack.config.js
@@ -3,6 +3,8 @@ const config = {
   mode: 'production',
   entry: {
     app: './src/js/app.js',
+    index: './src/js/index.js',
+    about: './src/js/about.js',
   },
   output: {
     filename: '[name].min.js',
