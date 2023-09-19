b7d0b3363e3c02bc02c502ccc0819d63ababdcac
Oleksandr Havryliuk <deolis.ua@gmail.com>
2023-09-19 16:17:06 +0300

    update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,7 @@ import { counterOnScroll } from './modules/counter';
 import { handleInsertProducts } from './modules/handleInsertProducts';
 import { insertProducts } from './modules/insertProducts';
 import { swiper } from './modules/swiper-testimonial';
+import { toggleBtnStatus } from './modules/toggleBtnStatus';
 import { getProducts } from './services/getProducts';
 
 counterOnScroll();
@@ -17,24 +18,39 @@ loadMoreBtn.addEventListener('click', loadMoreProducts);
 let itemsToShow = 8;
 const loadMoreQty = 8;
 
-// on load more
-function loadMoreProducts() {
-  getProducts().then(products => {
-    insertProducts(
-      products,
-      itemsToShow,
-      itemsToShow + loadMoreQty,
-      productsList
-    );
-
-    itemsToShow += loadMoreQty;
-
-    if (itemsToShow >= products.length) {
-      loadMoreBtn.style.display = 'none';
-    }
-  });
-}
-
 await handleInsertProducts(0, 8, productsList);
 await handleInsertProducts(12, 16, offersList);
 cart();
+
+// on load more
+function loadMoreProducts() {
+  const cartItems = JSON.parse(localStorage.getItem('products')) || [];
+
+  getProducts()
+    .then(products => {
+      insertProducts(
+        products,
+        itemsToShow,
+        itemsToShow + loadMoreQty,
+        productsList
+      );
+
+      itemsToShow += loadMoreQty;
+
+      if (itemsToShow >= products.length) {
+        loadMoreBtn.style.display = 'none';
+      }
+    })
+    .then(() => {
+      const addToCartBtn = document.querySelectorAll('.product__button');
+
+      addToCartBtn.forEach(button => {
+        const productId = button.parentNode.parentNode.id;
+        const alreadyInCart = cartItems.some(item => item.id === productId);
+
+        if (alreadyInCart) {
+          toggleBtnStatus(productId);
+        }
+      });
+    });
+}
